a309727a023b8a76c0d01bbc080c5181a47424ae
Prasanna Mestha <prasanna@betterhalf.ai>
2019-12-31 17:19:15 +0530

    Add api

--- public/js/google-map.js
+++ public/js/google-map.js
@@ -1,37 +1,9 @@
 function initialize() {
-    var locations = [
-        ['Kundalahalli, Bengaluru', 12.955997, 77.714816, 3],
-        ['Marathalli, Bengaluru', 12.959761, 77.701480, 2],
-        ['Hoodi, Bengaluru', 12.963765, 77.710610, 1]
-    ];
+    var locations = [];
 
     window.map = new google.maps.Map(document.getElementById('map'), {
         mapTypeId: google.maps.MapTypeId.ROADMAP
     });
-
-    var infowindow = new google.maps.InfoWindow();
-
-    var bounds = new google.maps.LatLngBounds();
-
-    for (i = 0; i < locations.length; i++) {
-        marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i][1], locations[i][2]),
-            icon: "./img/marker-icon.png",
-            //icon: "https://developers.google.com/maps/documentation/javascript/examples/full/images/library_maps.png",
-            map: map
-        });
-
-        bounds.extend(marker.position);
-
-        google.maps.event.addListener(marker, 'click', (function (marker, i) {
-            return function () {
-                infowindow.setContent(locations[i][0]);
-                infowindow.open(map, marker);
-            }
-        })(marker, i));
-    }
-
-    map.fitBounds(bounds);
 
     var listener = google.maps.event.addListener(map, "idle", function () {
         map.setZoom(14);
